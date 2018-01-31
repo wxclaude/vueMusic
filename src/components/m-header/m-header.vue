64ccdb44b855b01173061ef11199d82b614828ae
wangxiang <261676607@qq.com>
2018-01-31 23:22:23 +0800

    头部模块

---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="m-header">
+    <div class="icon"></div>
+    <div class="text">音乐播放器</div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default{}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+</style>
