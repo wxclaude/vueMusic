c64b36a4a87a096d7158ecbba7c5916fe77f6543
wangxiang <261676607@qq.com>
2018-02-05 00:43:43 +0800

    配好头部路由

--- src/main.js
+++ src/main.js
@@ -1,18 +1,26 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import 'babel-polyfill'
+//import 'common/js/hack'
 import Vue from 'vue'
 import App from './App'
-// import router from './router'
+import router from '@/router/index'
 import fastclick from 'fastclick'
-
-Vue.config.productionTip = false
+import VueLazyload from 'vue-lazyload'
+//import store from './store'
 
 import 'common/stylus/index.styl'
+/* eslint-disable no-unused-vars */
+// import vConsole from 'vconsole'
 
 fastclick.attach(document.body)
+
+Vue.use(VueLazyload, {
+  loading: require('common/image/default.png')
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render:h => h(App)
+  router,
+  //store,
+  render: h => h(App)
 })
