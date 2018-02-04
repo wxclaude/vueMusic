b489ea7fa4294092114134ea9a11b657c9ffd0ae
wangxiang <261676607@qq.com>
2018-02-04 23:08:02 +0800

    添加别名

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -35,7 +35,9 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'src': resolve('src'),
-      'common': resolve('src/common')
+      'common': resolve('src/common'),
+      'components': resolve('src/components'),
+      'api': resolve('src/api')
     }
   },
   module: {
