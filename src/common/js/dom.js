1ab52f4e544d190bdf9c1dceeb6d5fea3f54ae11
wangxiang <261676607@qq.com>
2018-02-10 23:56:21 +0800

    添加常用方法

---
+++ src/common/js/dom.js
@@ -0,0 +1,12 @@
+export function addClass (el, className) {
+  if (hasClass(el, className)) {
+    return
+  }
+  let newClass = el.className.split(' ')
+  newClass.push(className)
+  el.className = newClass
+}
+
+export function hasClass (el, className) {
+  return new RegExp('(^|\\s)' + className + '(\\s|$)').test(el.className)
+}
