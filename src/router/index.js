ded4249aa6c24e4aea72bf836c2deb3004939575
wangxiang <261676607@qq.com>
2018-02-22 23:13:26 +0800

    添加歌手router

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,66 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Recommend from 'components/recommend/recommend'
-import Singer from 'components/singer/singer'
-import Rank from 'components/rank/rank'
-import Search from 'components/search/search'
+
 Vue.use(Router)
+
+const Recommend = () => import('components/recommend/recommend')
+const Singer = () => import('components/singer/singer')
+const Rank = () => import('components/rank/rank')
+const Search = () => import('components/search/search')
+const SingerDetail = () => import('components/singer-detail/singer-detail')
+const Disc = () => import('components/disc/disc')
+// const TopList = () => import('components/top-list/top-list')
+// const UserCenter = () => import('components/user-center/user-center')
 
 export default new Router({
   routes: [
-    {path: '/', redirect: '/recommend'},
-    {path: '/recommend', component: Recommend},
-    {path: '/singer', component: Singer},
-    {path: '/rank', component: Rank},
-    {path: '/search', component: Search}
+    {
+      path: '/',
+      redirect: '/recommend'
+    },
+    {
+      path: '/recommend',
+      component: Recommend,
+      children: [
+        {
+          path: ':id',
+          component: Disc
+        }
+      ]
+    },
+    {
+      path: '/singer',
+      component: Singer,
+      children: [
+        {
+          path: ':id',
+          component: SingerDetail
+        }
+      ]
+    },
+    {
+      path: '/rank',
+      component: Rank,
+      children: [
+        {
+          path: ':id',
+          component: TopList
+        }
+      ]
+    },
+    {
+      path: '/search',
+      component: Search,
+      children: [
+        {
+          path: ':id',
+          component: SingerDetail
+        }
+      ]
+    },
+    {
+      path: '/user',
+      component: UserCenter
+    }
   ]
 })
