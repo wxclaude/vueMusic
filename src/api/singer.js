4b6e035f6134a999da80523de16b798872745a2f
wangxiang <261676607@qq.com>
2018-02-25 22:10:11 +0800

    获取歌手的歌曲数据

--- src/api/singer.js
+++ src/api/singer.js
@@ -1,6 +1,6 @@
 import jsonp from 'common/js/jsonp'
 import {commonParams, options} from './config'
-
+// 获取歌手的列表数据
 export function getSingerList() {
   const url = 'https://c.y.qq.com/v8/fcg-bin/v8.fcg'
 
@@ -17,7 +17,7 @@ export function getSingerList() {
 
   return jsonp(url, data, options)
 }
-
+// 获取歌手的歌曲数据
 export function getSingerDetail(singerId) {
   const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg'
 
