00633aeac24d1f21e7858ced6136066a38731ac6
wangxiang <261676607@qq.com>
2018-03-11 02:52:31 +0800

    优化推荐页面

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -37,21 +37,26 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import Slider from 'base/slider/slider'
   import Loading from 'base/loading/loading'
   import Scroll from 'base/scroll/scroll'
-  import Slider from 'base/slider/slider'
-  import { getRecommend, getDiscList } from 'api/recommend'
-  import { ERR_OK } from 'api/config'
+  import {getRecommend, getDiscList} from 'api/recommend'
+  import {playlistMixin} from 'common/js/mixin'
+  import {ERR_OK} from 'api/config'
+  import {mapMutations} from 'vuex'
+
   export default {
-    created () {
-      this._getRecommend()
-      this._getDiscList()
-    },
-    data () {
+    mixins: [playlistMixin],
+    data() {
       return {
         recommends: [],
         discList: []
       }
+    },
+    created() {
+      this._getRecommend()
+
+      this._getDiscList()
     },
     activated() {
       setTimeout(() => {
@@ -59,38 +64,53 @@
       }, 20)
     },
     methods: {
-      _getRecommend () {
-        getRecommend().then((res) => {
-          if (res.code === ERR_OK) {
-            this.recommends = res.data.slider
-          }
-        })
+      handlePlaylist(playlist) {
+        const bottom = playlist.length > 0 ? '60px' : ''
+
+        this.$refs.recommend.style.bottom = bottom
+        this.$refs.scroll.refresh()
       },
-      _getDiscList () {
-        getDiscList().then((res) => {
-          if (res.code === ERR_OK) {
-            this.discList = res.data.list
-          }
-        })
-      },
-      loadImage () {
+      loadImage() {
         if (!this.checkloaded) {
           this.checkloaded = true
           setTimeout(() => {
             this.$refs.scroll.refresh()
           }, 20)
         }
-      }
+      },
+      selectItem(item) {
+        this.$router.push({
+          path: `/recommend/${item.dissid}`
+        })
+        this.setDisc(item)
+      },
+      _getRecommend() {
+        getRecommend().then((res) => {
+          if (res.code === ERR_OK) {
+            this.recommends = res.data.slider
+          }
+        })
+      },
+      _getDiscList() {
+        getDiscList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.discList = res.data.list
+          }
+        })
+      },
+      ...mapMutations({
+        setDisc: 'SET_DISC'
+      })
     },
     components: {
       Slider,
-      Scroll,
-      Loading
+      Loading,
+      Scroll
     }
   }
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus">
+<style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
 
   .recommend
