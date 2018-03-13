0c4cae8436a1f35f6635e15f27d5ca8115c1e621
wangxiang <261676607@qq.com>
2018-03-14 01:15:22 +0800

    修改数据地址

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -15,7 +15,6 @@ const devWebpackConfig = merge(baseWebpackConfig, {
   },
   // cheap-module-eval-source-map is faster for development
   devtool: config.dev.devtool,
-  disableHostCheck:true,
   // these devServer options should be customized in /config/index.js
   devServer: {
     before(app) {
